b28c127473ad0974c8faea9905376f04e1b02b79
cdtinney <colintinney@gmail.com>
2019-03-21 21:21:44 +0800

    refactor: extract contact components for reuse

---
+++ src/components/contact/Phone.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Phone() {
+  return (
+    <React.Fragment>
+      <h5>PHONE</h5>
+      <p style={{ marginBottom: 0 }}>
+        705 927 1256
+        <br />
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default Phone;
