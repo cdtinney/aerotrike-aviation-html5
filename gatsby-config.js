e0c31aa213b5437e9f6d45e836d106e2428c2b96
cdtinney <colintinney@gmail.com>
2019-04-12 08:57:47 +0800

    seo: add robots txt plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,8 +5,17 @@ module.exports = {
     description: 'Ultralight flight training at its best.',
   },
   plugins: [
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-eslint',
+    'gatsby-plugin-sitemap', {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://aerotrikeaviation.com',
+        sitemap: 'https://aerotrikeaviation.com/sitemap.xml',
+        policy: [{
+          userAgent: '*',
+          allow: '/',
+        }],
+      },
+    }, 'gatsby-plugin-eslint',
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
